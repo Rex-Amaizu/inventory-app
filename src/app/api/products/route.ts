a5b16582c36f6f2af6f18a6c21058c01f0e17ec8
MacBook <rex.ebuka@sterling.ng>
2024-09-08 04:34:12 +0100

    fix: fixed caching issues

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,18 +1,20 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import clientPromise from "../lib/mongodb";
 import { dbName } from "@/app/api/config/keys";
 import { ProductWithInventory } from "../lib/models/Product";
-import { NextApiRequest, NextApiResponse } from "next";
 import { revalidateTag } from "next/cache";
 
-export async function GET(res: NextApiResponse) {
+export async function GET(req: NextRequest) {
   const client = await clientPromise;
   const db = client.db(dbName);
-  // Set cache control headers
-  res.setHeader("Cache-Control", "no-store");
+  // Set cache control headers using NextResponse
+  const response = NextResponse.json({});
+
+  response.headers.set("Cache-Control", "no-store");
   const productsCollection = db.collection<ProductWithInventory>("products");
 
   const products = await productsCollection.find({}).toArray();
+
   revalidateTag("products");
 
   return NextResponse.json(products);
