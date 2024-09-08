5f3e2a803eb0b0b1ab52669103d48d962ccdd230
MacBook <rex.ebuka@sterling.ng>
2024-09-08 04:51:33 +0100

    fix: fixed caching issues

--- src/components/products/Product.tsx
+++ src/components/products/Product.tsx
@@ -78,30 +78,30 @@ const Product: React.FC<ProductProps> = ({
 
   return (
     <div className={styles.container}>
-      <div className="flex justify-self-start w-[20px] sm:w-[30px] ms:w-[50px] font-semibold ms:font-medium text-black text-[5px] sm:text-[7px] ms:text-[10px] md:text-xs">
+      <div className="flex justify-self-start w-[20px] sm:w-[30px] ms:w-[50px] font-bold ms:font-medium text-black text-[7px] ms:text-[10px] md:text-xs">
         {sn}
       </div>
       <hr />
-      <div className="flex justify-self-start w-[50px] sm:w-[50px] ms:w-[80px] font-semibold ms:font-medium text-black text-[5px] sm:text-[7px] ms:text-[10px] md:text-xs">
+      <div className="flex justify-self-start w-[50px] sm:w-[50px] ms:w-[80px] font-bold ms:font-medium text-black text-[7px] ms:text-[10px] md:text-xs">
         {name}
       </div>
       <hr />
-      <div className="flex justify-self-start w-[80px] sm:w-[120px] ms:w-[160px] font-semibold ms:font-medium text-black text-[5px] sm:text-[7px] ms:text-[10px] md:text-xs">
+      <div className="flex justify-self-start w-[80px] sm:w-[120px] ms:w-[160px] font-bold ms:font-medium text-black text-[7px] ms:text-[10px] md:text-xs">
         {description}
       </div>
       <hr />
-      <div className="flex justify-self-start w-[30px] ms:w-[50px] font-semibold ms:font-medium text-black text-[5px] sm:text-[7px] ms:text-[10px] md:text-xs">
+      <div className="flex justify-self-start w-[30px] ms:w-[50px] font-bold ms:font-medium text-black text-[7px] ms:text-[10px] md:text-xs">
         ${price}
       </div>
       <hr />
-      <div className="flex justify-self-start w-[30px] ms:w-[50px] font-semibold ms:font-medium text-black text-[5px] sm:text-[7px] ms:text-[10px] md:text-xs">
+      <div className="flex justify-self-start w-[30px] ms:w-[50px] font-bold ms:font-medium text-black text-[7px] ms:text-[10px] md:text-xs">
         {stock}
       </div>
       <hr />
       {page === "home" && (
         <div
           onClick={openInventoryListModal}
-          className="flex justify-self-start items-center justify-center cursor-pointer w-[50px] sm:w-[80px] ms:w-[100px] h-5 sm:h-[30px] ms:h-[40px] rounded bg-[#6d31edff] font-semibold ms:font-medium text-white text-[5px] sm:text-[7px] ms:text-[10px] md:text-xs"
+          className="flex justify-self-start items-center justify-center cursor-pointer w-[60px] sm:w-[80px] ms:w-[100px] h-5 sm:h-[30px] ms:h-[40px] rounded bg-[#6d31edff] font-bold ms:font-medium text-white text-[7px] ms:text-[10px] md:text-xs"
         >
           View Inventory
         </div>
@@ -109,13 +109,13 @@ const Product: React.FC<ProductProps> = ({
       {page === "products" && (
         <HiOutlineDotsHorizontal
           onClick={openEditModal}
-          className="flex justify-self-start w-[50px] sm:w-[80px] ms:w-[100px] h-[30px] ms:h-[40px] cursor-pointer font-semibold ms:font-medium text-[#6d31edff] text-[5px] sm:text-[7px] ms:text-[10px] md:text-xs"
+          className="flex justify-self-start w-[60px] sm:w-[80px] ms:w-[100px] h-[30px] ms:h-[40px] cursor-pointer font-bold ms:font-medium text-[#6d31edff] text-[7px] ms:text-[10px] md:text-xs"
         />
       )}
       {page === "inventory" && (
         <HiOutlineDotsHorizontal
           onClick={openInventoryModal}
-          className="flex justify-self-start w-[50px] sm:w-[80px] ms:w-[100px] h-[30px] ms:h-[40px] cursor-pointer font-semibold ms:font-medium text-[#6d31edff] text-[5px] sm:text-[7px] ms:text-[10px] md:text-xs"
+          className="flex justify-self-start w-[60px] sm:w-[80px] ms:w-[100px] h-[30px] ms:h-[40px] cursor-pointer font-bold ms:font-medium text-[#6d31edff] text-[7px] ms:text-[10px] md:text-xs"
         />
       )}
       <EditProductModal
--- src/components/products/ProductHeader.tsx
+++ src/components/products/ProductHeader.tsx
@@ -4,22 +4,22 @@ import styles from "@/styles/products/ProductHeader.module.css";
 const ProductHeader = () => {
   return (
     <div className={styles.header}>
-      <div className="flex justify-self-start w-[20px] sm:w-[30px] ms:w-[50px] font-medium text-white text-[6px] sm:text-[8px] ms:text-xs md:text-sm">
+      <div className="flex justify-self-start w-[20px] sm:w-[30px] ms:w-[50px] font-bold sm:font-medium text-white text-[7px] sm:text-[8px] ms:text-xs md:text-sm">
         S/N
       </div>
-      <div className="flex justify-self-start w-[40px] sm:w-[50px] ms:w-[80px] font-medium text-white text-[6px] sm:text-[8px] ms:text-xs md:text-sm">
+      <div className="flex justify-self-start w-[40px] sm:w-[50px] ms:w-[80px] font-bold sm:font-medium text-white text-[7px] sm:text-[8px] ms:text-xs md:text-sm">
         Name
       </div>
-      <div className="flex justify-self-start w-[80px] sm:w-[120px] ms:w-[160px] font-medium text-white text-[6px] sm:text-[8px] ms:text-xs md:text-sm">
+      <div className="flex justify-self-start w-[80px] sm:w-[120px] ms:w-[160px] font-bold sm:font-medium text-white text-[7px] sm:text-[8px] ms:text-xs md:text-sm">
         Description
       </div>
-      <div className="flex justify-self-start w-[30px] ms:w-[50px] font-medium text-white text-[6px] sm:text-[8px] ms:text-xs md:text-sm">
+      <div className="flex justify-self-start w-[30px] ms:w-[50px] font-bold sm:font-medium text-white text-[7px] sm:text-[8px] ms:text-xs md:text-sm">
         Price
       </div>
-      <div className="flex justify-self-start w-[30px] ms:w-[50px] font-medium text-white text-[6px] sm:text-[8px] ms:text-xs md:text-sm">
+      <div className="flex justify-self-start w-[30px] ms:w-[50px] font-bold sm:font-medium text-white text-[7px] sm:text-[8px] ms:text-xs md:text-sm">
         Stock
       </div>
-      <div className="flex justify-self-start w-[50px] sm:w-[80px] ms:w-[100px] font-medium text-white text-[6px] sm:text-[8px] ms:text-xs md:text-sm">
+      <div className="flex justify-self-start w-[50px] sm:w-[80px] ms:w-[100px] font-bold sm:font-medium text-white text-[7px] sm:text-[8px] ms:text-xs md:text-sm">
         Actions
       </div>
     </div>
